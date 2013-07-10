6058b490748ba5608ae4f5d43575dc5ac9c77c98
Wendy Langer <lollipopenator@gmail.com>
2013-07-10 14:07:00 +0000

    ...with any luck, working now ...still testing hard-coded again to see why app now seems broken

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-//var response_buffer= fs.readFileSync("index.var");
-//html response_string = response_buffer.app();
+var response_buffer= fs.readFileSync("index.html");
+var response_string = response_buffer.app();
 
 app.get('/', function(request, response) {
-  /*response.send(response_string);*/
-  response.send("huh?");  
+  response.send(response_string);
+  //response.send("huh?");  
 });
 
 var port = process.env.PORT || 5000;
